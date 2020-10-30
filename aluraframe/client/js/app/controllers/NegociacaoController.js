9e5d948268ab36b86fbe6199fac9bd75b265cb2e
LeoPavani <leonardopavani.lp@hotmail.com>
2020-10-30 07:34:09 -0300

    terceira aula

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -7,13 +7,32 @@ class NegociacaoController{
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
 
     adiciona(event){
         event.preventDefault();
 
-        console.log(typeof(this._inputData.value));
-        console.log(this._inputData.value);
+
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao(){
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+    }
+
+    _limpaFormulario(){
+        this._inputData.value = '';
+        this._inputData.focus();
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = '';
     }
 }
